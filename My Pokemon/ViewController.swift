cf0292fbd491648fd8ce520a890148522010ec05
Keketso Ramosedi <kekeramosedi@gmail.com>
2022-10-30 22:33:53 +0200

    Initial Commit

---
+++ My Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Pokemon
+//
+//  Created by Keketso Ramosedi on 2022/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
