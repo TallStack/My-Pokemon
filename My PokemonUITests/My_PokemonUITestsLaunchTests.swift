4f5cf4af92d1ab95431e48e6db7582ad9508791f
Keketso Ramosedi <kekeramosedi@gmail.com>
2022-11-04 09:38:17 +0200

    Added new targets for unit tests and ui tests to project

---
+++ My PokemonUITests/My_PokemonUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  My_PokemonUITestsLaunchTests.swift
+//  My PokemonUITests
+//
+//  Created by Keketso Ramosedi on 2022/11/04.
+//  Copyright © 2022 TallStack Game Studios. All rights reserved.
+//
+
+import XCTest
+
+final class My_PokemonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
